7898053f19125118697db8cce1b6d096f86fe911
r b <brittonr@hotmail.co.uk>
2022-08-17 16:26:25 +0100

    Updates

---
+++ precache-manifest.d7aa819fad6b486bdbf20ecfd7f7b5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d92626d40813b561144",
+    "url": "/toDoList/static/css/main.818feeee.chunk.css"
+  },
+  {
+    "revision": "8d92626d40813b561144",
+    "url": "/toDoList/static/js/main.8d92626d.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/toDoList/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "6c76f8591af4ea5fe936",
+    "url": "/toDoList/static/js/2.6c76f859.chunk.js"
+  },
+  {
+    "revision": "813ddb2413a461aef196",
+    "url": "/toDoList/static/js/runtime~main.813ddb24.js"
+  },
+  {
+    "revision": "8a59e20cda37745270b0f28cbd3b11ef",
+    "url": "/toDoList/index.html"
+  }
+];
